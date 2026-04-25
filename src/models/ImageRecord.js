d8f9a92c7def283cf298a37c2789cd310552d0fb
cheehgovala <167295934+cheehgovala@users.noreply.github.com>
2026-04-25 18:55:54 +0200

    Delete src directory

--- src/models/ImageRecord.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const ImageRecordSchema = new mongoose.Schema({
-  filename: { type: String, required: true },
-  url: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-export const ImageRecord = mongoose.model('ImageRecord', ImageRecordSchema);
